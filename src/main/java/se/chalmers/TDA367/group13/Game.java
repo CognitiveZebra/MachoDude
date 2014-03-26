fe0048474399a7638e96dbc99da0af8b4fae017d
CognitiveZebra <mikael.lonn@outlook.com>
2014-03-26 17:37:51 +0100

    added runnable program using maven

---
+++ src/main/java/se/chalmers/TDA367/group13/Game.java
@@ -0,0 +1,43 @@
+package se.chalmers.TDA367.group13;
+
+import org.newdawn.slick.AppGameContainer;
+import org.newdawn.slick.BasicGame;
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.SlickException;
+
+public class Game extends BasicGame {
+
+    /** Screen width */
+    private static final int WIDTH = 800;
+    /** Screen height */
+    private static final int HEIGHT = 600;
+    
+    /** A counter... */
+    private int counter;
+
+    public Game() {
+        super("MachoDude");
+    }
+
+    public void render(GameContainer container, Graphics g) throws SlickException {
+
+    }
+
+    @Override
+    public void init(GameContainer container) throws SlickException {
+    }
+
+    @Override
+    public void update(GameContainer container, int delta) throws SlickException {
+    }
+    
+    public static void main(String[] args) throws SlickException {
+        AppGameContainer app = new AppGameContainer(new Game());
+        app.setDisplayMode(WIDTH, HEIGHT, false);
+        app.setForceExit(false);
+        app.setTargetFrameRate(60);
+        app.start();
+    }
+
+}
