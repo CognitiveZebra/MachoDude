6ce0023d9e93f2709f513f8727158cdf3cda7c9a
Branne93 <alebra@student.chalmers.se>
2014-03-29 12:48:58 +0100

    Refactored entities for more efficient usage

--- src/main/java/se/chalmers/TDA367/group13/entities/Entities.java
+++ src/main/java/se/chalmers/TDA367/group13/entities/Entities.java
@@ -1,69 +1,30 @@
 package se.chalmers.TDA367.group13.entities;
 
+import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.geom.Rectangle;
 
-public abstract class Entities {
-	private float x, y, height, width;
+public abstract class Entities extends Rectangle {
 	private Image image;
-	private Rectangle border;
 	
 	public Entities (float x, float y, Image image){
-		this.x = x;
-		this.y = y;
+		super(x, y, image.getWidth(), image.getHeight());
 		this.image = image;
-		this.height = image.getHeight();
-		this.width = image.getWidth();
-		border = new Rectangle (x, y, height, width);
-		
 	}
 
-	public float getX() {
-		return x;
-	}
-
-	public void setX(float x) {
-		this.x = x;
-		update();
-	}
-
-	public float getY() {
-		return y;
-	}
-
-	public void setY(float y) {
-		this.y = y;
-		update();
-	}
-
-	public float getHeight() {
-		return height;
-	}
-
-	public void setHeight(float height) {
-		this.height = height;
-		update();
-	}
-
-	public float getWidth() {
-		return width;
-	}
-
-	public void setWidth(float width) {
-		this.width = width;
-		update();
-	}
-
-	public Image getImage() {
+	public Image getImage()
+	{
 		return image;
 	}
-
-	public void setImage(Image image) {
+	
+	public void setImage(Image image)
+	{
 		this.image = image;
+		setBounds(getX(), getY(), image.getWidth(), image.getHeight());
 	}
 	
-	public void update(){
-		border.setBounds(x, y, height, width);
-	}
+	public void draw(Graphics g){ 
+		g.drawImage(image, getX(), getY()); 
+		}
 
 }
