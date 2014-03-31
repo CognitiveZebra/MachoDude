ee0eb717f6a93033481b26d7b671526a1df39dc3
Anders Stigsson <CognitiveZebra@dhcp-191236.eduroam.chalmers.se>
2014-03-31 16:04:02 +0200

    Added the content of GameStateController

--- src/main/java/se/chalmers/TDA367/group13/GameStateController.java
+++ src/main/java/se/chalmers/TDA367/group13/GameStateController.java
@@ -1,5 +1,34 @@
 package se.chalmers.TDA367.group13;
 
-public class GameStateController {
+import org.newdawn.slick.GameContainer;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.state.StateBasedGame;
+
+
+public class GameStateController extends StateBasedGame {
+
+	private static GameState gameState;
+
+
+	
+	
+	public GameStateController(String name) {
+		super(name);
+	}
+
+	@Override
+	public void initStatesList(GameContainer arg0) throws SlickException {
+		
+		gameState = new GameState();
+
+		
+		
+		addState(gameState);
+	}
+	
+	public static GameState getGameState() {
+		return gameState;
+	}
+
 
 }
