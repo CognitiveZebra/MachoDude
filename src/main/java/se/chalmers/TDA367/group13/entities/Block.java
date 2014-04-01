0a1b9fd743c67c45439bd2cd798faf5938b4251a
Mikael Lönn <mikael.lonn@outlook.com>
2014-04-01 23:27:58 +0200

    Added class describing blocks on level.

---
+++ src/main/java/se/chalmers/TDA367/group13/entities/Block.java
@@ -0,0 +1,13 @@
+package se.chalmers.TDA367.group13.entities;
+import org.newdawn.slick.Image;
+
+
+public class Block extends Entity{
+
+	public Block(float x, float y, Image image) {
+		super(x, y, image);
+	}
+	
+	//TODO 
+
+}
