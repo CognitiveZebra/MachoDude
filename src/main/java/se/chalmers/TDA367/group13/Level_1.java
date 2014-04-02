37479969e62a9f8ed3bfeb4f7b0e100a449f19bf
Mikael Lönn <mikael.lonn@outlook.com>
2014-04-02 12:49:40 +0200

    Added contructor for Level_1

--- src/main/java/se/chalmers/TDA367/group13/Level_1.java
+++ src/main/java/se/chalmers/TDA367/group13/Level_1.java
@@ -1,5 +1,12 @@
 package se.chalmers.TDA367.group13;
 
-public class Level_1 {
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+import org.newdawn.slick.tiled.TiledMap;
 
+public class Level_1 extends Level{
+
+	public Level_1()throws SlickException {
+		super(new Camera(), new TiledMap("res/Maps/test_map.tmx"), new Image("res/Backgrounds/Jungle_Test.gif"));
+	}
 }
