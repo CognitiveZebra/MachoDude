59502d54d9734081fab1c21763a8c9382377b0ee
Alvmo <alexanderalvmo@live.com>
2014-04-03 12:48:04 +0200

    Tweaked jump update in Game Model

--- src/main/java/se/chalmers/TDA367/group13/GameModel.java
+++ src/main/java/se/chalmers/TDA367/group13/GameModel.java
@@ -16,84 +16,85 @@ public class GameModel {
 	private GameContainer container;
 	private Level level;
 	private Player player;
-	
+
 	public GameModel(GameContainer gc) {
 		this.container = gc;
-		
+
 		try {
 			level = new Level_1();
-			player = new Player(300, 300, "res/Sprites/MachoDude/sheet.png",  "res/Sprites/MachoDude/sheet.xml");
+			player = new Player(300, 300, "res/Sprites/MachoDude/sheet.png",
+					"res/Sprites/MachoDude/sheet.xml");
 		} catch (SlickException e) {
 			e.printStackTrace();
 		}
-		
-	}
-	
-	public void update (Input input){
-		Rectangle nextXPos = new Rectangle(player.getX(), player.getY(), player.getWidth(), player.getHeight());
 
-		
-		if(input.isKeyDown(Input.KEY_A)){
+	}
+
+	public void update(Input input) {
+		Rectangle nextXPos = new Rectangle(player.getX(), player.getY(),
+				player.getWidth(), player.getHeight());
+
+		if (input.isKeyDown(Input.KEY_A)) {
 			nextXPos.setX(player.nextLeftX());
-			if(isLegal(level.getBlocks(), nextXPos)){
+			if (isLegal(level.getBlocks(), nextXPos)) {
 				player.moveLeft();
 			}
-		} else if(input.isKeyDown(Input.KEY_D)){
+		} else if (input.isKeyDown(Input.KEY_D)) {
 			nextXPos.setX(player.nextRightX());
-			if(isLegal(level.getBlocks(), nextXPos)){
-				
-				if(nextXPos.getCenterX() > (container.getWidth()/2)){
+			if (isLegal(level.getBlocks(), nextXPos)) {
+				if (nextXPos.getCenterX() > (container.getWidth() / 2)) {
 					level.moveBlocks(player.getX() - nextXPos.getX());
 					level.getCamera().move(player.getX() - nextXPos.getX());
 				} else {
 					player.moveRight();
 				}
-			} 
+			}
 		}
-		
-		if(input.isKeyDown(Input.KEY_W)){
-			if(player.getState() != State.JUMPING){
+
+		if (input.isKeyDown(Input.KEY_W)) {
+			if (player.getState() != State.JUMPING) {
 				player.setState(State.JUMPING);
 				player.setJumpStart(System.currentTimeMillis());
 				player.setJumpCharge(System.currentTimeMillis());
-			} else if(System.currentTimeMillis() - player.getJumpCharge() < 500) {
+			} else if (System.currentTimeMillis() - player.getJumpCharge() < 500) {
 				player.setJumpStart(System.currentTimeMillis());
 			}
 		} else {
 			player.setJumpCharge(0);
 		}
-		
 
-		
-		Rectangle nextYPos = new Rectangle(player.getX(), player.getY(), player.getWidth(), player.getHeight());
+		Rectangle nextYPos = new Rectangle(player.getX(), player.getY(),
+				player.getWidth(), player.getHeight());
 		player.updateYVelocity();
 		nextYPos.setY(player.nextY());
-		if(isLegal(level.getBlocks(), nextYPos)){
+		if (isLegal(level.getBlocks(), nextYPos)) {
 			player.moveY();
-		} else {
-			if(input.isKeyDown(Input.KEY_D)||input.isKeyDown(Input.KEY_A)){
+		} else if(!input.isKeyDown(Input.KEY_W)) {
+ 				player.setState(State.STILL);
+		}
+		
+		if(player.getState() != State.JUMPING){
+			if(input.isKeyDown(Input.KEY_D) || input.isKeyDown(Input.KEY_A)) {
 				player.setState(State.WALKING);
-			} else {
-				player.setState(State.STILL);
 			}
-		}		
+		}
+
 	}
-	
-	public boolean isLegal(LinkedList<Block> blocks, Rectangle hitbox){
-		for(Block b : blocks){
-			if(hitbox.intersects(b)){
+
+	public boolean isLegal(LinkedList<Block> blocks, Rectangle hitbox) {
+		for (Block b : blocks) {
+			if (hitbox.intersects(b)) {
 				return false;
 			}
 		}
-	
 		return (hitbox.getX() > 0);
 	}
-	
-	public Level getLevel(){
+
+	public Level getLevel() {
 		return level;
 	}
-	
-	public Player getPlayer(){
+
+	public Player getPlayer() {
 		return player;
 	}
 }
