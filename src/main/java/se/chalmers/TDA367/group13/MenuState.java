1e02d6057b14e93aa8568cdcc418263774d8c1ed
Alvmo <alexanderalvmo@live.com>
2014-04-08 13:22:27 +0200

    Created simple menu state that show background and enters game when enter key is pressed

--- src/main/java/se/chalmers/TDA367/group13/MenuState.java
+++ src/main/java/se/chalmers/TDA367/group13/MenuState.java
@@ -1,22 +1,33 @@
 package se.chalmers.TDA367.group13;
 
+import org.newdawn.slick.Color;
 import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.Input;
 import org.newdawn.slick.SlickException;
-import  org.newdawn.slick.state.BasicGameState;
+import org.newdawn.slick.state.BasicGameState;
 import org.newdawn.slick.state.StateBasedGame;
+import org.newdawn.slick.state.transition.FadeInTransition;
+import org.newdawn.slick.state.transition.FadeOutTransition;
 
 public class MenuState extends BasicGameState {
+	public static final int ID = 2;
+	Image background; 
+	Input input;
 
 	@Override
 	public void init(GameContainer gc, StateBasedGame sbg)
 			throws SlickException {
+		background = new Image("res/Backgrounds/Jungle_Test.gif");
+		input = gc.getInput();
 		
 	}
 
 	@Override
 	public void render(GameContainer gc, StateBasedGame sbg, Graphics g)
 			throws SlickException {
+		g.drawImage(background,0,0);
 		
 	}
 
@@ -24,11 +35,15 @@ public class MenuState extends BasicGameState {
 	public void update(GameContainer gc, StateBasedGame sbg, int delta)
 			throws SlickException {
 		
+		
+		if(input.isKeyDown(Input.KEY_ENTER)){
+			sbg.enterState(GameStateController.getGameState().getID());
+		}
 	}
 
 	@Override
 	public int getID() {
-		return 0;
+		return ID;
 	}
 
 }
