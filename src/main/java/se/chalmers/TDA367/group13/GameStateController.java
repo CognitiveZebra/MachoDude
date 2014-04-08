70a8b4411398d539795f6f67b236be60df10602e
Alvmo <alexanderalvmo@live.com>
2014-04-08 13:21:26 +0200

    Added a menu state to GameStateController

--- src/main/java/se/chalmers/TDA367/group13/GameStateController.java
+++ src/main/java/se/chalmers/TDA367/group13/GameStateController.java
@@ -6,11 +6,9 @@ import org.newdawn.slick.state.StateBasedGame;
 
 
 public class GameStateController extends StateBasedGame {
-
+	private static MenuState menuState;
 	private static GameState gameState;
 
-
-	
 	
 	public GameStateController(String name) {
 		super(name);
@@ -18,17 +16,20 @@ public class GameStateController extends StateBasedGame {
 
 	@Override
 	public void initStatesList(GameContainer arg0) throws SlickException {
-		
+		menuState = new MenuState();
 		gameState = new GameState();
 
 		
-		
+		addState(menuState);
 		addState(gameState);
 	}
 	
 	public static GameState getGameState() {
 		return gameState;
 	}
-
+	
+	public static MenuState getMenuState() {
+		return menuState;
+	}
 
 }
