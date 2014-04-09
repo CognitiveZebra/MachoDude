e7053b99b375a1ee8e9afc4cb1c5d30e06189722
Mikael Lönn <mikael.lonn@outlook.com>
2014-04-09 15:13:46 +0200

    Fixed arm placement on emeny

--- src/main/java/se/chalmers/TDA367/group13/entities/Enemy_1.java
+++ src/main/java/se/chalmers/TDA367/group13/entities/Enemy_1.java
@@ -4,16 +4,23 @@ import org.newdawn.slick.Animation;
 import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
+import org.newdawn.slick.geom.Point;
+
+import se.chalmers.TDA367.group13.entities.Player.Direction;
 
 
 public class Enemy_1 extends Enemy{
 
 	private Image[] right, left, standLeft, standRight;
 	private Animation stillLeft, stillRight, walkLeft, walkRight;
+	private Point rightShoulder, leftShoulder;
 	private float walkingspeed = 1;
 
 	public Enemy_1(float x, float y) throws SlickException {
 		super(x, y, "res/Sprites/Enemies/Enemy_1/Enemy_1.png", "res/Sprites/Enemies/Enemy_1/sheet.xml");
+		weapon = new Enemy_1_Weapon(x, y);
+		rightShoulder = new Point(6, 13);
+		leftShoulder = new Point(11, 13);
 		initAnimations();
 	}
 	
@@ -53,6 +60,19 @@ public class Enemy_1 extends Enemy{
 			break;
 		}
 		g.drawAnimation(animation, getX(), getY());
+		g.drawRect(weapon.getX(), weapon.getY(), weapon.getWidth(), weapon.getHeight());
+		
+		if (direction == Direction.LEFT){
+			weapon.setCenterX(x + leftShoulder.getX());
+			weapon.setCenterY(y + leftShoulder.getY());
+		//	weapon.setImage(weapon.getLeftImage());
+		}
+		else{
+		//	weapon.setImage(weapon.getRightImage());
+			weapon.setCenterX(x + rightShoulder.getX());
+			weapon.setCenterY(y + rightShoulder.getY());
+		}
+		weapon.render(g);
 	}
 	
 	@Override
@@ -60,7 +80,10 @@ public class Enemy_1 extends Enemy{
 		standLeft =  resizeImages(standLeft, scale);
 		standRight = resizeImages(standRight, scale);
 		right = resizeImages(right, scale); 
-		left = resizeImages(left, scale); 
+		left = resizeImages(left, scale);
+		leftShoulder.setLocation(leftShoulder.getX() * scale, leftShoulder.getY() *scale);
+		rightShoulder.setLocation(rightShoulder.getX() * scale, rightShoulder.getY() *scale);
+		weapon.resize(scale);
 		
 		setImage(standRight[0]);
 	}
