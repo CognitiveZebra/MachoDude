81aa88109718e4910f3e5a7a4a69b0e148dae292
Alexander Branzell <alebra@student.chalmers.se>
2014-04-09 16:47:02 +0200

    Added cooldown to testing weapon

--- src/main/java/se/chalmers/TDA367/group13/entities/TestWeapon.java
+++ src/main/java/se/chalmers/TDA367/group13/entities/TestWeapon.java
@@ -4,13 +4,23 @@ import org.newdawn.slick.Image;
 import org.newdawn.slick.SlickException;
 
 public class TestWeapon extends Weapon {
+	private long time;
+	private float cooldown ;
+	
 	
 	TestWeapon(float x, float y) throws SlickException {
 		super(x, y, new Image("res/Sprites/testArm.png").getFlippedCopy(true, false), "TestWeapon", 1);
+		time = System.currentTimeMillis();
+		cooldown = 300;
 	}
 	
 	public void fireWeapon(){
+
+		if ((System.currentTimeMillis()-time) > cooldown){
 		getProjectiles().add(new Projectile(x, y, getImage().copy(), getAngle(), 10));
+		time = System.currentTimeMillis();
+		}
+		
 	}
 
 }
