7719b6784190a2f15a63ffb0c5fa38bbed5ea8a4
Alexander Branzell <alebra@student.chalmers.se>
2014-04-14 15:42:15 +0200

    Added destructable block

---
+++ src/main/java/se/chalmers/TDA367/group13/entities/DestructableBlock.java
@@ -0,0 +1,28 @@
+package se.chalmers.TDA367.group13.entities;
+
+import org.newdawn.slick.Image;
+
+public class DestructableBlock extends Block {
+	private float health;
+	private float maxHealth;
+
+	public DestructableBlock(float x, float y, Image image, float health) {
+		super(x, y, image);
+		this.maxHealth = health;
+		this.health = health;
+	}
+	
+	public void loseHealth(){	
+		health--;
+	}
+	
+	public boolean isDestroyed() {
+		return health <= 0;
+	}
+	
+	
+	public boolean isHurt(){
+		return health < maxHealth;
+	}
+
+}
