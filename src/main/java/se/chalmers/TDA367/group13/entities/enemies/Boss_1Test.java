f1eaf424ce04528e856c1ca27d843a5b4631fcb2
Anders Stigsson <andstig@student.chalmers.se>
2014-05-08 12:10:49 +0200

    Added Boss_1Test to get some help from fellow collaborators

---
+++ src/main/java/se/chalmers/TDA367/group13/entities/enemies/Boss_1Test.java
@@ -0,0 +1,116 @@
+package se.chalmers.TDA367.group13.entities.enemies;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Boss_1Test {
+
+	Boss_1 test = new Boss_1(0,0);
+	
+	@Test
+	public void testSetY() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testRenderGraphics() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testResize() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testBoss_1() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testInitAnimations() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetHealth() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetHealthInt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testResizeBeam() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFireLaser() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSetHealthFloat() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testResizeImages() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testLoseHealth() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsDestroyed() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsHurt() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetWalkingSpeed() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNextRightX() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNextLeftX() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetNextY() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMoveRight() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMoveLeft() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testMoveY() {
+		fail("Not yet implemented");
+	}
+
+}
