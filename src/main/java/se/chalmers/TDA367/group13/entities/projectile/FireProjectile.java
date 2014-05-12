d3f0cae489b78624b5ece864f4ad15e712e8488f
Alvmo <alexanderalvmo@live.com>
2014-05-12 14:42:53 +0200

    Refactored FireProjectile

--- src/main/java/se/chalmers/TDA367/group13/entities/projectile/FireProjectile.java
+++ src/main/java/se/chalmers/TDA367/group13/entities/projectile/FireProjectile.java
@@ -1,5 +1,6 @@
 package se.chalmers.TDA367.group13.entities.projectile;
 
+import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
 
 import se.chalmers.TDA367.group13.particles.ParticleFactory;
@@ -13,5 +14,10 @@ public class FireProjectile extends ParticleProjectile {
 		damage = 1;
 		particleEmitter = ParticleFactory.createEmitter("fire");
 		ps.addEmitter(particleEmitter);
+	}
+	
+	@Override 
+	public void render(Graphics g){
+		ps.render();
 	}
 }
