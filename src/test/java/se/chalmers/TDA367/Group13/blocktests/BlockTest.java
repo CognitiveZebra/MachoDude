a8b9a345e4972c36e168260a950a5671aa6182e7
Anders Stigsson <andstig@student.chalmers.se>
2014-05-12 15:06:15 +0200

    Fixed blocktests with extremely ugly code

--- src/test/java/se/chalmers/TDA367/Group13/blocktests/BlockTest.java
+++ src/test/java/se/chalmers/TDA367/Group13/blocktests/BlockTest.java
@@ -2,13 +2,39 @@ package se.chalmers.TDA367.Group13.blocktests;
 
 import static org.junit.Assert.*;
 
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
 import org.junit.Test;
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.GLContext;
+import org.newdawn.slick.Image;
+import org.newdawn.slick.SlickException;
+
+import se.chalmers.TDA367.group13.entities.Entity;
+import se.chalmers.TDA367.group13.entities.block.Block;
 
 public class BlockTest {
 
+	Block test;
+	
+	@Before
+	public void setUp() throws Exception{
+
+		Display.create();
+		test = new Block(1, 1, new Image("res/Sprites/testArm.png"));
+
+	}
 	@Test
 	public void testBlock() {
-		//TODO: Figure out a test for this
+
+		assertTrue(test instanceof Entity);
+	}
+	
+	@AfterClass
+	public static void close(){
+		Display.destroy();
 	}
 
 }
