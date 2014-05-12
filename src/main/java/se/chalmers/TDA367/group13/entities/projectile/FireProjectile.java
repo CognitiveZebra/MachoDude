2e0ea99638fa048ec18c06ae60e78ce5115ea2dc
Alvmo <alexanderalvmo@live.com>
2014-05-12 14:38:30 +0200

    Added ParticleProjectile

--- src/main/java/se/chalmers/TDA367/group13/entities/projectile/FireProjectile.java
+++ src/main/java/se/chalmers/TDA367/group13/entities/projectile/FireProjectile.java
@@ -1,22 +1,17 @@
 package se.chalmers.TDA367.group13.entities.projectile;
 
-import org.newdawn.slick.Graphics;
 import org.newdawn.slick.Image;
-import org.newdawn.slick.particles.ConfigurableEmitter;
-import org.newdawn.slick.particles.ParticleSystem;
 
 import se.chalmers.TDA367.group13.particles.ParticleFactory;
 import se.chalmers.TDA367.group13.util.Direction;
 
-public class PlayerProjectile extends ParticleProjectile {
+public class FireProjectile extends ParticleProjectile {
 
-
-	
-	public PlayerProjectile(float x, float y, Image image, float angle,
+	public FireProjectile(float x, float y, Image image, float angle,
 			float speed, Direction direction) {
 		super(x, y, image, angle, speed, direction);
 		damage = 1;
-		particleEmitter = ParticleFactory.createEmitter("smoke");
+		particleEmitter = ParticleFactory.createEmitter("fire");
 		ps.addEmitter(particleEmitter);
 	}
 }
