b19474ed5f91d9ebfeb96753feebf3c8d26fe8b3
Anders Stigsson <andstig@student.chalmers.se>
2014-05-13 09:27:27 +0200

    Added some tests on Enemy_1

--- src/test/java/se/chalmers/TDA367/Group13/entitytests/Enemy_1Test.java
+++ src/test/java/se/chalmers/TDA367/Group13/entitytests/Enemy_1Test.java
@@ -1,46 +1,48 @@
 package se.chalmers.TDA367.Group13.entitytests;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
 
+import org.junit.BeforeClass;
 import org.junit.Test;
+import org.lwjgl.opengl.Display;
+
+import se.chalmers.TDA367.group13.entities.Entity;
+import se.chalmers.TDA367.group13.entities.enemies.Enemy;
+import se.chalmers.TDA367.group13.entities.enemies.enemy1.Enemy_1;
 
 public class Enemy_1Test {
 
-	//TODO: Need to get around problem with SlickException before these tests will work.
+	static Enemy_1 test;
 	
-	@Test
-	public void testRenderGraphics() {
-		fail("Not yet implemented");
+	@BeforeClass
+	public static void startUp() throws Exception{
+		Display.create();
+		test = new Enemy_1(1, 1);
 	}
 
 	@Test
 	public void testResize() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetWalkingSpeed() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testGetWeapon() {
-		fail("Not yet implemented");
+		float height = test.getHeight();
+		test.resize(3);
+		assertTrue(test.getHeight() == 3*height);
 	}
 
 	@Test
 	public void testEnemy_1() {
-		fail("Not yet implemented");
+		assertTrue(test instanceof Enemy && test instanceof Entity);
 	}
 
 	@Test
 	public void testInitAnimations() {
-		fail("Not yet implemented");
+		float height = test.getHeight();
+		test.initAnimations();
+		assertTrue(test.getHeight() == 2*height);
 	}
 
 	@Test
 	public void testResizeImages() {
-		fail("Not yet implemented");
+		//TODO: Discuss how to test resizeImage
 	}
 
 }
