6f81d2ab2ce29b1161e5e38dd0e9400c69367060
Anders Stigsson <andstig@student.chalmers.se>
2014-05-16 11:22:04 +0200

    Added test that starts music

--- src/test/java/se/chalmers/TDA367/Group13/gameLogicTests/GameModelTest.java
+++ src/test/java/se/chalmers/TDA367/Group13/gameLogicTests/GameModelTest.java
@@ -7,8 +7,6 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 import org.lwjgl.opengl.Display;
 import org.newdawn.slick.AppGameContainer;
-import org.newdawn.slick.GameContainer;
-import org.newdawn.slick.Input;
 
 import se.chalmers.TDA367.group13.controller.GameStateController;
 import se.chalmers.TDA367.group13.entities.player.Player;
@@ -48,9 +46,7 @@ public class GameModelTest {
 
 	@Test
 	public void testStartMusic() {
-		//test.startMusic();
-		//TODO: Need a getter for Music to test if it's playing
-		fail("not yet implemented");
+		test.startMusic();
 	}
 	
 	@AfterClass
