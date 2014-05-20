df44d71f112add340da4b17fb1b993f123ea785f
Anders Stigsson <andstig@student.chalmers.se>
2014-05-20 11:55:13 +0200

    Added a suite to start all tests in an easy way

---
+++ src/test/java/se/chalmers/TDA367/Group13/main/AllTests.java
@@ -0,0 +1,37 @@
+package se.chalmers.TDA367.Group13.main;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+import se.chalmers.TDA367.Group13.blocktests.BlockTest;
+import se.chalmers.TDA367.Group13.blocktests.DestructableBlockTest;
+import se.chalmers.TDA367.Group13.entitytests.Boss1ProjectileTest;
+import se.chalmers.TDA367.Group13.entitytests.Boss_1Test;
+import se.chalmers.TDA367.Group13.entitytests.Enemy1ProjectileTest;
+import se.chalmers.TDA367.Group13.entitytests.Enemy_1Test;
+import se.chalmers.TDA367.Group13.entitytests.Enemy_2Test;
+import se.chalmers.TDA367.Group13.entitytests.FireProjectileTest;
+import se.chalmers.TDA367.Group13.entitytests.HealthBarEnemyTest;
+import se.chalmers.TDA367.Group13.entitytests.PlayerProjectileTest;
+import se.chalmers.TDA367.Group13.entitytests.ProjectileImageFactoryTest;
+import se.chalmers.TDA367.Group13.gameLogicTests.GameModelTest;
+import se.chalmers.TDA367.Group13.gameLogicTests.GameTest;
+import se.chalmers.TDA367.Group13.stateTests.GameOverStateTest;
+import se.chalmers.TDA367.Group13.stateTests.GameStateControllerTest;
+import se.chalmers.TDA367.Group13.stateTests.GameStateTest;
+import se.chalmers.TDA367.Group13.stateTests.MenuStateTest;
+import se.chalmers.TDA367.Group13.stateTests.QuitStateTest;
+import se.chalmers.TDA367.Group13.stateTests.SettingsStateTest;
+import se.chalmers.TDA367.Group13.stateTests.StatsStateTest;
+
+@RunWith(Suite.class)
+@SuiteClasses({ BlockTest.class, DestructableBlockTest.class, Boss_1Test.class, Boss1ProjectileTest.class, Enemy_1Test.class,
+	Enemy_2Test.class, Enemy1ProjectileTest.class,
+	FireProjectileTest.class, HealthBarEnemyTest.class,
+	PlayerProjectileTest.class, ProjectileImageFactoryTest.class, GameModelTest.class, GameTest.class, GameOverStateTest.class, GameStateControllerTest.class,
+	GameStateTest.class, MenuStateTest.class, QuitStateTest.class,
+	SettingsStateTest.class, StatsStateTest.class })
+public class AllTests {
+
+}
